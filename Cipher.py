b3a76575b3d5ea83a92948e43aeeebcfdd0cfcce
salar8676 <sksalarshahprince@gmail.com>
2024-10-15 16:42:06 +0500

    Update Cipher.py

--- Cipher.py
+++ Cipher.py
@@ -1,12 +1,30 @@
 def main():
-  user_input = input('Enter Input: ')
-  shift = int(input('Enter shift: '))
-  shift= shift%26
-  name = encoder1(user_input, shift)
-  print(name)
-  name1= decoder1(name, shift)
+  import pyfiglet
 
+  text="Cipher"
+  welcome=pyfiglet.figlet_format(text,font='graffiti')
+  print(welcome)
+  while True:
+    from EncodeDecode import encoder1,decoder1
+    option = input('Encode or Decode (E/D/Exit): ').lower()
 
+    if option == 'e':
+      user_input = input('Enter Input: ')
+      shift = int(input('Enter shift: '))
+      shift= shift%26
+      name = encoder1(user_input, shift)
+      print(name)
+      continue
+    elif option == 'd':
+      user_input = input('Enter Input: ')
+      shift = int(input('Enter shift: '))
+      shift= shift%26
+      decoded = decoder1(user_input, shift)
+      print(decoded)
+      continue
+    else:
+      print('Good Bye')
+      break
 
 
 
